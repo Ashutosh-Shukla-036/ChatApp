78bc15e38a63a79eea0aa17bce7dff3e159272b3
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-17 20:57:59 +0530

    Updated userModel

--- BackEnd/models/userModel.js
+++ BackEnd/models/userModel.js
@@ -1,11 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },  // Unique username
+    password: { type: String, required: true },  // Required password
 });
 
-const User = mongoose.model("User", userSchema);
-
+const User = mongoose.model('UserTable', userSchema);
 export default User;

8b262d1909922287d980e3a981de9432e4051b18
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-18 10:23:21 +0530

    Added App.jsx file with routing

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { SocketProvider } from "./component/SocketContext";
+import { Navbar } from "./pages/navbar";
+import { Home } from "./pages/Home";
+import { SignUp } from "./pages/Signup";
+import { Login } from "./pages/Login";
+import { AllUsers } from "./pages/Alluser";
+import { Chat } from "./component/Chat";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <> 
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+const App = () => (
+    <>
+      <Navbar />
+        <div className="containe mx-auto p-4">
+          <SocketProvider>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/allusers" element={<AllUsers />}></Route>
+              <Route path="/chat/:username" element={<Chat />}></Route>
+            </Routes>
+          </SocketProvider>
+        </div>
     </>
-  )
-}
+);
 
-export default App
+export default App;
